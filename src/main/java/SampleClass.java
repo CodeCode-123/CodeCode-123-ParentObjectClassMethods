a847157e26b6e243805963ac14e24d8891956da8
Shen Tang <adatang23@yahoo.com>
2025-06-04 22:04:58 +0000

    completed the Parent Object Class Methods Lab

--- src/main/java/SampleClass.java
+++ src/main/java/SampleClass.java
@@ -63,9 +63,15 @@ public class SampleClass {
     public boolean b;
 
     //    implement a custom .equals(SampleClass other){} method here.
+    public boolean equals(SampleClass other) {
+        return this.a == other.a && this.b == other.b;
+    }
 
 
     //    implement a custom .toString(){} method here.
+    public String toString() {
+        return "a: " + a + ", b: " + b;
+    }
 
     
 }
